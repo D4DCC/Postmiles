8b0920aad39a1b0d06920dcf658b8036f33b6e8e
Dewhurst <s141846@dot.ca.gov>
2018-10-04 15:28:52 -0700

    First commit

--- JavaScript/Practice2.js
+++
@@ -1,7 +0,0 @@
-let divCont = document.getElementById("content");
-    divCont.style.border = "1px solid lightgrey";
-let searchPM = document.getElementById('searchPM');
-    searchPM.style.overflow = "scroll";
-    searchPM.style.height = "17.25rem";
-let tData = document.getElementById('tData');
-let pOut = document.getElementById('postmileOutput');
